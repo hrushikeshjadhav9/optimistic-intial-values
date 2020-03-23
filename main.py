9d8d4a3ebccb412533f16e6fed9a72c9038760fe
Hrushikesh Jadhav <hrishikesh.jadhav28@gmail.com>
2020-03-23 13:44:30 +0530

    code corrections

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 class Slot_machine():
     """ a simple slot machine """
 
-    def __init__(self, mean):
+    def __init__(self, mean, upper_limit):
         """
 
         :param mean: actual win rate
@@ -13,7 +13,7 @@ class Slot_machine():
         N: number of plays a the slot machine
         """
         self.mean = mean
-        self.mean_estimate = 0
+        self.mean_estimate = upper_limit
         self.N = 0
         self.x = 0
 
@@ -31,63 +31,37 @@ class Slot_machine():
 
 
 
-def experiment(N, eps, *means):
+def experiment(N, upper_limit,  *means):
     """ compares epsilon values given by user """
-    all_sm = []
-    for mean in means:
-        all_sm.append(Slot_machine(mean))
+    all_sm = [Slot_machine(mean, upper_limit) for mean in means]
 
-    # choosing a random initial sm
-    # current_best_sm = random.choice(all_sm)
-    current_best_sm = all_sm[0]
+    # records actual mean estimates of pulled slot machines
+    all_pulls = np.empty(N)
 
-    # records actual mean values of pulled slot machines
-    all_pulls = []
+    for i in range(N):
 
-    # records random values generated that decide between exploration / exploitation
-    all_values = []
+        index = np.argmax([sm.mean_estimate for sm in all_sm])
+        x = all_sm[index].pull()
+        all_pulls[i] = x
 
-    current_best_values = []
+        # Tracking progress through time
+        cumulative_average = np.cumsum(all_pulls)/(np.arange(N)+1)
 
-    for _ in range(N):
-        value = random.uniform(0,1)
-        all_values.append(value)
-
-        if value < eps:
-            random_sm = random.choice(all_sm)
-            random_sm.pull()
-            all_pulls.append(random_sm.mean_estimate)
-
-        else:
-            index = np.argmax([sm.mean_estimate for sm in all_sm])
-            current_best_sm = all_sm[index]
-            current_best_sm.pull()
-            all_pulls.append(current_best_sm.mean_estimate)
-
-        current_best_values.append(current_best_sm.mean_estimate)
-
-    return current_best_values, all_pulls
+    return all_pulls, cumulative_average
 
 
-N = 100
-eps03 = experiment(N, 0.03, 0.1, 0.3, 0.5)
-eps05 = experiment(N, 0.05, 0.1, 0.3, 0.5)
-eps1 = experiment(N, 0.1, 0.1, 0.3, 0.5)
+if __name__ == "__main__":
 
-plt.plot(eps03[0], label = "eps = 0.03")
-plt.plot(eps05[0], label = "eps = 0.05")
-plt.plot(eps1[0], label = "eps = 0.1")
-plt.legend()
+    # Number of plays / pulls.
+    N = 1000
+    pulls, cumulative_average = experiment(N, 1, 0.1, 0.3, 0.5)
 
-plt.ylabel("best mean estimate")
-plt.xlabel("iteration")
-plt.show()
+    # plt.plot(pulls)
+    # plt.ylabel("all slot machine pulls")
+    # plt.xlabel("iteration")
+    # plt.show()
 
-plt.plot(eps03[1], label = "eps = 0.03")
-plt.plot(eps05[1], label = "eps = 0.05")
-plt.plot(eps1[1], label = "eps = 0.1")
-plt.legend()
-
-plt.ylabel("pulls")
-plt.xlabel("iteration")
-plt.show()
+    plt.plot(cumulative_average)
+    plt.ylabel("cumulative average")
+    plt.xlabel("pull count")
+    plt.show()
